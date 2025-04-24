cc1669939f7abdc1b47f231594676485bf5ebe3b
NaveeraRajChuhan <nnfstars@gmail.com>
2025-04-24 19:57:05 +0500

    finish

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-var num
+// var num = Math.random()
+// document.write(num)
+
+
+// var userinp = prompt('Write hat or tail')
+
+// var num = (Math.round(Math.random()))
+// document.write(num)
+// if(num===0 || num===1){
+//     document.write("you win")
+//     if(num===0){
+//         document.write(hats)
+//     } if(num===1){
+//         document.write(tail)
+//     }
+// }
+
+// else if(num===0){
+//     document.write(hats)
+// }else if(num===1){
+//     document.write(tail)
+// }
+// else{
+//     tail
+// }
+
+
+// var hi = Math.round(Math.random());
+// document.write(hi);
+
+var tail = prompt("enter tail name")
+var head = prompt("enter head name")
+
+var answer = Math.round(Math.random())
+
+if (answer === 0) {
+    document.write(head + " wins")
+} else {
+    document.write(tail + " wins")
+    
+}
